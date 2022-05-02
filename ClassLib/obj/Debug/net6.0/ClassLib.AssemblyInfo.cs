9d662f9737d74244af07ce8f173c74d98a9f3488
MazzGuille <mazzcode@gmail.com>
2022-05-01 23:04:51 -0300

    build

--- ClassLib/obj/Debug/net6.0/ClassLib.AssemblyInfo.cs
+++ ClassLib/obj/Debug/net6.0/ClassLib.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CLassLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ClassLib")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ClassLib")]
+[assembly: System.Reflection.AssemblyProductAttribute("CLassLib")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CLassLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
